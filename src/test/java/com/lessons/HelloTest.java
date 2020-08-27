b753ef7956305e80bd769792bbedabfcb3a904bd
Khagan <azizovkhgn@gmail.com>
2020-08-27 23:12:39 +0400

    Gradle

--- src/test/java/com/lessons/HelloTest.java
+++ src/test/java/com/lessons/HelloTest.java
@@ -1,6 +1,11 @@
 package com.lessons;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 import java.io.*;
 
