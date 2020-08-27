c0abfe7d1ca223930b0c29943f671f1e6cc2582f
Khagan <azizovkhgn@gmail.com>
2020-08-27 23:53:45 +0400

    Gradle

--- src/main/java/com/lessons/Main.java
+++ src/main/java/com/lessons/Main.java
@@ -3,6 +3,6 @@ package com.lessons;
 public class Main {
 
     public static void main(final String[] args) {
-        System.out.print("Hello world!\n");
+        System.out.print("wrong world!\n");
     }
 }
